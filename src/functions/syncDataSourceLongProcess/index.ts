80e41d389cb61af8b13e1777a56c182962d90675
mbret <bret.maxime@gmail.com>
2022-04-23 17:08:34 +0200

    fixed cors

--- src/functions/syncDataSourceLongProcess/index.ts
+++ src/functions/syncDataSourceLongProcess/index.ts
@@ -3,12 +3,4 @@ import { handlerPath } from '@libs/handler-resolver';
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   role: "lambdaDefault",
-  events: [
-    {
-      http: {
-        method: 'post',
-        path: 'refresh-metadata',
-      },
-    },
-  ],
 };
